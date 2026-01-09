6b544805400fa800ba5c8f881830f553a7abd29b
Monika09-byte <monika.matkar24@vit.edu>
2026-01-09 19:44:18 +0530

    Add delete task functionaliity to UI

--- ui.py
+++ ui.py
@@ -1,20 +1,11 @@
 import tkinter as tk
 
-# -------------------
-# Application State
-# -------------------
 tasks = []
 
-# -------------------
-# Main Window
-# -------------------
 root = tk.Tk()
 root.title("To-Do App")
 root.geometry("400x300")
 
-# -------------------
-# Widgets
-# -------------------
 label = tk.Label(root, text="Welcome to To-Do App")
 label.pack()
 
@@ -25,19 +16,29 @@ def add_task():
     task = entry.get()
 
     if task.strip() == "":
-        return  # ignore empty input
+        return
 
     tasks.append(task)
     task_listbox.insert(tk.END, task)
     entry.delete(0, tk.END)
 
+def delete_task():
+    selected = task_listbox.curselection()
+
+    if not selected:
+        return
+
+    index = selected[0]
+    task_listbox.delete(index)
+    tasks.pop(index)
+
 add_button = tk.Button(root, text="Add Task", command=add_task)
 add_button.pack()
+
+delete_button = tk.Button(root, text="Delete Task", command=delete_task)
+delete_button.pack()
 
 task_listbox = tk.Listbox(root)
 task_listbox.pack()
 
-# -------------------
-# Event Loop
-# -------------------
 root.mainloop()
