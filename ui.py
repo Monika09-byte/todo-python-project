84a2bc1d40b25a8e7de3fd4ea02e1e8efd8915a3
Monika09-byte <monika.matkar24@vit.edu>
2026-01-09 20:53:55 +0530

    Update Tkinter UI

--- ui.py
+++ ui.py
@@ -1,25 +1,18 @@
 import tkinter as tk
-import todo   # import logic module
+import todo
 
-# --------------------
-# Main Window
-# --------------------
+todo.load_tasks()
+
 root = tk.Tk()
 root.title("To-Do App")
 root.geometry("400x350")
 
-# --------------------
-# Widgets
-# --------------------
 label = tk.Label(root, text="Welcome to To-Do App")
 label.pack(pady=5)
 
 entry = tk.Entry(root, width=30)
 entry.pack(pady=5)
 
-# --------------------
-# Functions (UI → Logic)
-# --------------------
 def add_task_ui():
     task = entry.get()
 
@@ -40,22 +33,16 @@ def delete_task_ui():
     todo.delete_task(index)
     task_listbox.delete(index)
 
-# --------------------
-# Buttons
-# --------------------
 add_button = tk.Button(root, text="Add Task", command=add_task_ui)
 add_button.pack(pady=5)
 
 delete_button = tk.Button(root, text="Delete Task", command=delete_task_ui)
 delete_button.pack(pady=5)
 
-# --------------------
-# Task Display
-# --------------------
 task_listbox = tk.Listbox(root, width=40, height=10)
 task_listbox.pack(pady=10)
 
-# --------------------
-# Event Loop
-# --------------------
+for task in todo.get_tasks():
+    task_listbox.insert(tk.END, task)
+
 root.mainloop()
